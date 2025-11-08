b6734faf2dc687016f1f6e9258d3eccac56d7a6a
Reaper-Official <reaper@etik.com>
2025-11-08 13:56:03 +0000

    sqd

--- shell.sh
+++ shell.sh
@@ -1,14 +1,1 @@
-cat > /workspace/Hiveup/requirements.txt <<'EOF'
-tqdm
-yt-dlp
-opencv-python
-torch
-easyocr
-pandas
-numpy
-requests
-EOF
-
-pip install -r /workspace/Hiveup/requirements.txt
-
-python -m yt_dlp --version
+pip install -r requirements.txt
